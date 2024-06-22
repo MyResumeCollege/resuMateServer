035915aabdacf1c03d1855b15ee71437fe79a972
hadarza <hadar.zaguri@gmail.com>
2024-06-22 18:13:51 +0300

    linkedin intgreation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import CvUploader from "./cvUploader";
+import linkedinProfile from "./linkedinData"
 
 // Export the base-router
 const baseRouter = Router();
 
 baseRouter.use("/cv", CvUploader);
+baseRouter.use("/linkedin", linkedinProfile)
 
 export default baseRouter;
