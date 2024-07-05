30e30727533fcdedf31a0025a758f6dcf8238fdf
hadarza <hadar.zaguri@gmail.com>
2024-07-05 18:37:37 +0300

    set image as not required

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -2,9 +2,9 @@ import mongoose from 'mongoose'
 
 export type User = {
   _id?: mongoose.Schema.Types.ObjectId
+  name: string
   email: string
   password: string
-  name: string
   image?: string
   isPremium: boolean
   refreshTokens?: string[]
