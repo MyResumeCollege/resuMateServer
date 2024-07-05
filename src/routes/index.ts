84ab47e0753315a04033b3bc94b4006201c85921
omerbenarieh <omerrbenarieh@gmail.com>
2024-07-05 16:38:02 +0300

    Working on the login

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 import CvUploader from './cvUploader'
 import CvGenerator from './cvGenerator'
 import linkedinProfile from './linkedinData'
+import LoginRouter from './login'
 
 const baseRouter = Router()
 
 baseRouter.use('/cv', CvUploader, CvGenerator)
 baseRouter.use('/linkedin', linkedinProfile)
+baseRouter.use('/login', LoginRouter)
 
 export default baseRouter
