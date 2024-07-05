dd0ff9c47cb8048851133d9c56e0b60df7f7ed0b
hadarza <hadar.zaguri@gmail.com>
2024-07-05 18:37:37 +0300

    set image as not required

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,13 +1,33 @@
-import mongoose from 'mongoose'
-import { User } from '../types/user.type'
+import mongoose from "mongoose";
+import { User } from "../types/user.type";
 
 const userSchema = new mongoose.Schema<User>({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  name: { type: String, required: true },
-  image: { type: String },
-  isPremium: { type: Boolean, required: true, default: false },
-  refreshTokens: [{ type: String }],
-})
+  name: { 
+    type: String, 
+    required: true 
+  },
+  email: { 
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true 
+   },
+  password: { 
+    type: String,
+    required: true 
+  },
+  image: { 
+    type: String,
+    required: false 
+  },
+  isPremium: { 
+    type: Boolean,
+    required: true,
+    default: false 
+  },
+  refreshTokens: [{ 
+    type: [String],
+  }],
+});
 
-export default mongoose.model<User>('User', userSchema)
+export default mongoose.model<User>("User", userSchema);
