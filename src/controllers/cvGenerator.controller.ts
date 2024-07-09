e0367d95ce69d91aef4d4b3382c69f75c04e6326
hadarza <hadar.zaguri@gmail.com>
2024-07-09 23:35:51 +0300

    add experience support from generate Resume

--- src/controllers/cvGenerator.controller.ts
+++ src/controllers/cvGenerator.controller.ts
@@ -2,12 +2,13 @@ import { Request, Response } from 'express'
 import { generateResume } from '../services/GroqAI.service'
 const generateResumeFromScratch = async (req: Request, res: Response) => {
   try {
-    const { name, job, description, skills } = req.body
+    const { name, job, description, skills, experiences } = req.body
     const CVUploadResponse = await generateResume({
       name,
       job,
       description,
-      skills
+      skills,
+      experiences
     })
     res.status(200).json({ CVTextContent: CVUploadResponse })
   } catch (err) {
