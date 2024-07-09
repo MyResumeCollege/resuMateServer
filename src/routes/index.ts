5d5dbb7f5f9e177ddfa03a13ca64cca28b066c5b
hadarza <hadar.zaguri@gmail.com>
2024-07-09 22:35:06 +0300

    revert changes - authRoute back

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import CvUploader from './cvUploader';
 import CvGenerator from './cvGenerator';
 import linkedinProfile from './linkedinData';
+import authRoute from "./auth";
 
 // Export the base-router
 const baseRouter = Router();
 
 baseRouter.use('/cv', CvUploader, CvGenerator);
 baseRouter.use('/linkedin', linkedinProfile);
+baseRouter.use("/auth", authRoute);
 
 export default baseRouter;
