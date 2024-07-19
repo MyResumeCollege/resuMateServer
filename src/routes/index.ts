537cd22012d713aa02559690cf9c465610150158
hadarza <hadar.zaguri@gmail.com>
2024-07-19 10:36:43 +0300

    premium endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,16 @@
-import { Router } from 'express';
-import CvUploader from './cvUploader';
-import CvGenerator from './cvGenerator';
-import linkedinProfile from './linkedinData';
+import { Router } from "express";
+import CvUploader from "./cvUploader";
+import CvGenerator from "./cvGenerator";
+import linkedinProfile from "./linkedinData";
+import user from "./user"
 import authRoute from "./auth";
 
 // Export the base-router
 const baseRouter = Router();
 
-baseRouter.use('/cv', CvUploader, CvGenerator);
-baseRouter.use('/linkedin', linkedinProfile);
+baseRouter.use("/cv", CvUploader, CvGenerator);
+baseRouter.use("/linkedin", linkedinProfile);
 baseRouter.use("/auth", authRoute);
+baseRouter.use("/user", user)
 
 export default baseRouter;
