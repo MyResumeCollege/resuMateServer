093334ef3c7d162cc60a8f5aaff5d9481f357f5f
hadarza <hadar.zaguri@gmail.com>
2024-07-20 19:22:14 +0300

    POC puppetter view

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import CvUploader from "./cvUploader";
 import CvGenerator from "./cvGenerator";
+import cvPreview from "./cvPreview"
 import linkedinProfile from "./linkedinData";
 import user from "./user"
 import authRoute from "./auth";
@@ -12,5 +13,6 @@ baseRouter.use("/cv", CvUploader, CvGenerator);
 baseRouter.use("/linkedin", linkedinProfile);
 baseRouter.use("/auth", authRoute);
 baseRouter.use("/user", user)
+baseRouter.use("/preview", cvPreview)
 
 export default baseRouter;
---
+++ src/services/generatePdf.service.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import puppeteer from "puppeteer";
+
+export const generatePdf = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { fullName } = req.body;
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    const url = `http://localhost:5173/preview?fullName=${encodeURIComponent(fullName)}`;
+
+    await page.goto(url,{
+        waitUntil: "networkidle2",
+      }
+    );
+
+    const pdfBuffer = await page.pdf({ format: "a4" });
+    await browser.close();
+
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename="cv.pdf"');    
+    res.send(pdfBuffer);
+  } catch (error) {
+    console.error("Error generating PDF:", error);
+    res.status(500).send("Error generating PDF");
+  }
+};
