c6ab4bc46fb4fc635555ad01a096d7e10e95dfbd
omerbenarieh <omerrbenarieh@gmail.com>
2024-07-23 18:51:30 +0300

    init translate

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,19 @@
-import { Router } from "express";
-import CvUploader from "./cvUploader";
-import CvGenerator from "./cvGenerator";
-import cvPreview from "./cvPreview"
-import linkedinProfile from "./linkedinData";
-import user from "./user"
-import authRoute from "./auth";
+import { Router } from 'express'
+import CvUploader from './cvUploader'
+import CvGenerator from './cvGenerator'
+import cvPreview from './cvPreview'
+import linkedinProfile from './linkedinData'
+import user from './user'
+import authRoute from './auth'
+import CvTranslator from './cvTranslator'
 
 // Export the base-router
-const baseRouter = Router();
+const baseRouter = Router()
 
-baseRouter.use("/cv", CvUploader, CvGenerator);
-baseRouter.use("/linkedin", linkedinProfile);
-baseRouter.use("/auth", authRoute);
-baseRouter.use("/user", user)
-baseRouter.use("/preview", cvPreview)
+baseRouter.use('/cv', CvUploader, CvGenerator, CvTranslator)
+baseRouter.use('/linkedin', linkedinProfile)
+baseRouter.use('/auth', authRoute)
+baseRouter.use('/user', user)
+baseRouter.use('/preview', cvPreview)
 
-export default baseRouter;
+export default baseRouter
