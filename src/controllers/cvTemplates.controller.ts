1853e3dad00acfc04da8edf049836f6d6ba0011b
omerbenarieh <omerrbenarieh@gmail.com>
2024-08-03 15:04:53 +0300

    fix templates

--- src/controllers/cvTemplates.controller.ts
+++ src/controllers/cvTemplates.controller.ts
@@ -1,41 +1,40 @@
-import path from "path";
-import Template from "../models/resumeTemplate";
-import { Request, Response } from "express";
-import express from "express";
+import path from 'path'
+import Template from '../models/resumeTemplate'
+import { Request, Response } from 'express'
+import express from 'express'
 
-const app = express();
-
-const BASE_URL = 'http://localhost:' + process.env.PORT
+const app = express()
+const PORT = process.env.PORT || 3000
+const BASE_URL = `http://localhost:${PORT}`
 const IMAGE_FOLDER = '/templates'
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')))
 
 const getTemplates = async (req: Request, res: Response) => {
   try {
-    const templates = await Template.find();
+    const templates = await Template.find()
 
     const templatesWithUrls = templates.map(template => ({
       ...template.toObject(),
       imageUrl: `${BASE_URL}${IMAGE_FOLDER}/${path.basename(
         template.imageUrl
-      )}`
+      )}`,
     }))
-
-    res.status(200).json(templatesWithUrls);
+    res.status(200).json(templatesWithUrls)
   } catch (error) {
-    res.status(500).json({ message: "Error fetching templates", error });
+    res.status(500).json({ message: 'Error fetching templates', error })
   }
-};
+}
 
 const addTemplate = async (req: Request, res: Response) => {
-  const { imageUrl, isPremium } = req.body;
-  const newTemplate = new Template({ imageUrl, isPremium });
+  const { imageUrl, isPremium } = req.body
+  const newTemplate = new Template({ imageUrl, isPremium })
   try {
-    await newTemplate.save();
-    res.status(201).json(newTemplate);
+    await newTemplate.save()
+    res.status(201).json(newTemplate)
   } catch (error) {
-    res.status(500).json({ message: "Error adding template", error });
+    res.status(500).json({ message: 'Error adding template', error })
   }
-};
+}
 
-export default { getTemplates, addTemplate };
+export default { getTemplates, addTemplate }
