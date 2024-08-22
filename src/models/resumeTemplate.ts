ba3c416bf2d6c1c7a3bd22ab154366a2dbef6dd3
Hadar Zaguri <48456834+hadarza@users.noreply.github.com>
2024-08-23 00:12:10 +0300

    remove templates from server - saved on client (#69)

--- src/models/resumeTemplate.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-import { ResumteTemplate } from '../types/resumeTemplate.type'
-
-const resumeTemplateSchema = new mongoose.Schema<ResumteTemplate>({
-  imageUrl: { type: String, required: true },
-  isPremium: { type: Boolean, default: false },
-})
-export default mongoose.model<ResumteTemplate>('Template', resumeTemplateSchema)
